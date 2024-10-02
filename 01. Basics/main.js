82609c6c7ac836cfe8b78bb213f69db1e8d6362e
FaisalMohammedElorm <mohammedelormf@gmail.com>
2024-10-02 20:38:29 +0100

    type conversion

--- 01. Basics/main.js
+++ 01. Basics/main.js
@@ -1,9 +1,1 @@
-let favActorFirstName = "Dwayne";
-let favActorLastName = "Johnson";
-
-let fullName = `${favActorFirstName} ${favActorLastName}`
-let upperCase = fullName.toUpperCase();
-let message = `My favourite Actor Is ${upperCase} and he is one of the best Action Movie figures in Hollywood,`;
-
-message += " his best show is Silicon Valley";
-console.log(message);
+// Type Conversion
