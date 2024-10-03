bbfdba56dffd41bf0bbbf2284f34dfc767b38bbc
FaisalMohammedElorm <mohammedelormf@gmail.com>
2024-10-03 11:39:23 +0100

    conditionals

--- 02. Control Flow/main.js
+++ 02. Control Flow/main.js
@@ -1,8 +1,11 @@
-let a = 10;
-let b = 20;
-
-if(a > b){
-  console.log("a is greater than b");
-} else if( a < b){
-  console.log("a is less than b");
+let password;
+if(password === 8){
+  console.log("Welcome");
+} else if(password <= 8){
+  console.log("Password is short");
+} else if(password >= 8){
+  console.log("Too Long Password");
+  console.log("Password should be 8 characters");
+} else {
+  console.log("Please provide a password");
 }
