6590851fe7b6e03b2b081cd2f286073b54caffde
FaisalMohammedElorm <mohammedelormf@gmail.com>
2024-10-03 12:06:56 +0100

    now at for loops

--- 02. Control Flow/main.js
+++ 02. Control Flow/main.js
@@ -1,16 +1,1 @@
-let fruit = "Banana";
-// Switch statement to check fruits
- switch(fruit){
-  case "Banana":
-    console.log("Banana is good");
-    break;
-  case "Orange":
-    console.log("I am not a fan of orange");
-    break;
-  case "Apple":
-    console.log("How you like them apples?");
-    break;
-  default:
-    console.log("I have never heard of that fruit.");
-
- }
+// For loops
