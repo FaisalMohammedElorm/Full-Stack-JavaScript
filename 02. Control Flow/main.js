5e9572872586b24f751d769c7b272c1ebcc22a94
FaisalMohammedElorm <mohammedelormf@gmail.com>
2024-10-03 20:17:22 +0100

    nested loop

--- 02. Control Flow/main.js
+++ 02. Control Flow/main.js
@@ -1,3 +1,9 @@
-for (let i = 0; i <= 105; i++ ){
-  console.log("Hello World", i);
+for (let i = 1; i <= 10; i++){
+  console.log("--- Outer Loop ---", i);
+  // Nested Loop
+  for(let j = 1; j <=5; j++){
+    console.log("--- Inner Loop ---", i);
+  }
+
+
 }
