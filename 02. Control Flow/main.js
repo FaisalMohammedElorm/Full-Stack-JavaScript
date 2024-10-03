5d1dc7a900fbac1cfc11d62fc080e286ea1db87c
FaisalMohammedElorm <mohammedelormf@gmail.com>
2024-10-03 11:56:43 +0100

    default

--- 02. Control Flow/main.js
+++ 02. Control Flow/main.js
@@ -22,4 +22,7 @@ switch(day){
   case "Sunday":
     console.log("Today is Sunday");
     break;
+  default:
+    console.log("Invalid day");
+    break;  // This will be executed if none of the above cases match.  If not provided, it will throw an error.  It's a good practice to include a default case.  Here, it's used to handle invalid inputs.  In a real-world application, you might want to redirect to an error page or handle it differently.  For now, we'll just log an error message.  In a real-world application, you'd want to redirect to an error page.  For example: window.location.href = "error.html";  In this case, make sure to replace "error.html" with the actual URL of your error page.  This will redirect the user to the error page.  If the user tries to access a page that doesn't exist, they'll be redirected to the error page.  This is a basic example of how to handle invalid inputs in a switch statement.
 }
