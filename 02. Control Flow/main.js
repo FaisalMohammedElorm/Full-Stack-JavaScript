4ef643afa6484eebe9b103d3de5f2ae0d8ff3f99
FaisalMohammedElorm <mohammedelormf@gmail.com>
2024-10-03 20:24:37 +0100

    for loops in javascript

--- 02. Control Flow/main.js
+++ 02. Control Flow/main.js
@@ -1,9 +1,4 @@
-for (let i = 1; i <= 10; i++){
-  console.log("--- Outer Loop ---", i);
-  // Nested Loop
-  for(let j = 1; j <=5; j++){
-    console.log("--- Inner Loop ---", i);
-  }
 
-
+for(i=0; i<=1000; i++){
+  console.log("Faisal", i);
 }
