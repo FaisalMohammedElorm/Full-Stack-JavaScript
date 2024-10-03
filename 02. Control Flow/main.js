ea749a7cdaef245aa8a93c4ba5e4308cc67eb14c
FaisalMohammedElorm <mohammedelormf@gmail.com>
2024-10-03 20:37:11 +0100

    do while exercise

--- 02. Control Flow/main.js
+++ 02. Control Flow/main.js
@@ -1,7 +1,13 @@
-// Do While 
-let i = 1;
+// // Do While 
+// let i = 1;
+
+// do {
+//   console.log("Hello World", i);
+//   i++;
+// } while (i<=5); 
+let i = 20;
 
 do {
-  console.log("Hello World", i);
+  console.log("Mohammed Faisal", i);
   i++;
-} while (i<=5); 
+} while(i<=400);
